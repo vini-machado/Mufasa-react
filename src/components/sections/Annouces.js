464c8606ffb0b4c8f1ea4c7dbce9b68d57665413
gumagu01 <gustavomaringoulart@gmail.com>
2020-11-11 15:55:00 -0300

    anúncios

---
+++ src/components/sections/Annouces.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import {Component} from 'react'
+import "../../containers/Annouces.css";
+export default class Annouces extends Component {
+    render(){
+      return (
+        <div class="boxed">
+            <h1>
+                Anúncios
+            </h1>
+            <h1>
+                Anúncios
+            </h1>
+        </div>
+      )}}
+
+
--- src/views/Home.js
+++ src/views/Home.js
@@ -5,13 +5,15 @@ import FeaturesTiles from '../components/sections/FeaturesTiles';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
 import Testimonial from '../components/sections/Testimonial';
 import Cta from '../components/sections/Cta';
-
+import Announces from '../components/sections/Annouces'
 const Home = () => {
 
   return (
     <>
       <Hero className="illustration-section-01" />
+      <Announces />
       <FeaturesTiles />
+      <Announces />
       <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
       <Testimonial topDivider />
       <Cta split />
